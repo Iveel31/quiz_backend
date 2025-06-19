5f70cff3657ce94a8f679a24ca72da917c2479f3
2goo_NO <143684446+2gooNo@users.noreply.github.com>
2025-06-19 19:29:47 +0800

    import bug fixed

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const connectDb = async () => {
-    await mongoose.connect("mongodb+srv://iveelturu:<db_password>@iveel.embu1nk.mongodb.net/");
-    console.log('All Database Connected');
+  try {
+    await mongoose.connect(
+      "mongodb+srv://iveelturu:iveel0131@iveel.embu1nk.mongodb.net/"
+    );
+    console.log("All Database Connected");
+  } catch (error) {
+    console.error("Database connection failed:", error.message);
+  }
 };
 connectDb();
 
 app.listen(8000, () => {
   console.log("http://localhost:8000/");
-    console.log("http://localhost:8000/");
 });
