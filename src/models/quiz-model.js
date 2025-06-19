72477b1ea8dee7122dec022ec802d6282b6ba4f6
2goo_NO <143684446+2gooNo@users.noreply.github.com>
2025-06-19 19:17:29 +0800

    by tugu

--- src/models/quiz-model.js
+++ src/models/quiz-model.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 const quizSchema = new mongoose.Schema({
-    answers:Array,
-    question:String,
-    correctAns:Number,
-    category:String
-})
-export const QuizModel = mongoose.model("quiz",quizSchema)
+  answers: Array,
+  question: String,
+  correctAns: Number,
+  category: String,
+});
+export const QuizModel = mongoose.model("quiz", quizSchema);
