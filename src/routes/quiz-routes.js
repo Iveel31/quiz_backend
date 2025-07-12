63ed33fe6a5e6bc72864938f5570b80bb551ee6d
2goo_NO <143684446+2gooNo@users.noreply.github.com>
2025-07-13 00:06:52 +0800

    tugu

--- src/routes/quiz-routes.js
+++ src/routes/quiz-routes.js
@@ -1,6 +1,10 @@
-import express from "express"
-import { getQuiz, createQuiz, getQuizByCat } from "../controllers/quiz-controller.js"
-export const quizRouter = express.Router()
-quizRouter.get('/quiz', getQuiz)
-quizRouter.post('/createQuiz', createQuiz)
-quizRouter.get('/QuizByCat', getQuizByCat)
+import express from "express";
+import {
+  getQuiz,
+  createQuiz,
+  getQuizByCat,
+} from "../controllers/quiz-controller.js";
+export const quizRouter = express.Router();
+quizRouter.get("/quiz", getQuiz);
+quizRouter.post("/createQuiz", createQuiz);
+quizRouter.post("/QuizByCat", getQuizByCat);
